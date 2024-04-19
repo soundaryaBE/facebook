1dfda6c2cdb15e1c8e4fe2a1df587fae35715443
soundaryaBE <soundaryaraj62@gmail.com>
2024-04-19 17:47:11 +0530

    commit 2

---
+++ FaceBook/src/test/java/logoutTest.java
@@ -0,0 +1,21 @@
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class logoutTest {
+
+@Test
+
+public void logout()
+{
+	WebDriver driver;
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.get("https://www.facebook.com/");
+	System.out.println("loggedout Successfully");
+}
+
+}
