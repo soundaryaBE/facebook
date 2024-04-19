15bf7a8f7c53d786b5496d7e3f1f6b7a80859359
soundaryaBE <soundaryaraj62@gmail.com>
2024-04-19 17:24:07 +0530

    1st commit

---
+++ FaceBook/src/test/java/loginTest.java
@@ -0,0 +1,22 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class loginTest {
+	WebDriver driver;
+@Test
+
+public void login()
+{
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.get("https://www.facebook.com/");
+	driver.findElement(By.id("email")).sendKeys("soundaryaraj62@gmail.com");
+	driver.findElement(By.id("pass")).sendKeys("Kanishk6$");
+	driver.findElement(By.name("login")).click();
+}
+}
